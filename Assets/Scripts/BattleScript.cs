3064f8f3fcf37903564165797126eba2e53e610f
toofantah <toofan.tah@gmail.com>
2021-05-21 22:29:50 +0300

    Update - Dead and Respawn after Seconds Method
    Dead and Respawn after Seconds Method added in Battle Script

--- Assets/Scripts/BattleScript.cs
+++ Assets/Scripts/BattleScript.cs
@@ -5,9 +5,14 @@ using UnityEngine.UI;
 using Photon.Pun;
 using TMPro;
 
-public class BattleScript : MonoBehaviour
+public class BattleScript : MonoBehaviourPun
 {
     public Spinner spinnerScript;
+    public GameObject uI_3DGameObject;
+    public GameObject deathPanelUIPrefab;
+    private GameObject _deathPanelUIGameObject;
+
+    private Rigidbody rb;
 
     private float _startSpinSpeed;
     private float _currentSpinSpeed;
@@ -16,6 +21,7 @@ public class BattleScript : MonoBehaviour
 
     public bool isAttacker;
     public bool isDefender;
+    private bool _isDead = false;
 
     [Header("Player Type Damage Coefficient")]
     public float doDamageCoefficient_Attacker = 10.0f;
@@ -92,24 +98,106 @@ public class BattleScript : MonoBehaviour
     [PunRPC]
     public void DoDamage( float _damageAmount)
     {
-        if (isAttacker)
+        if (!_isDead)
         {
-            _damageAmount *= getDamageCoefficient_Attacker;
+            if (isAttacker)
+            {
+                _damageAmount *= getDamageCoefficient_Attacker;
+            }
+            else if (isDefender)
+            {
+                _damageAmount *= getDamageCoefficient_Defender;
+            }
+            spinnerScript.spinSpeed -= _damageAmount;
+            _currentSpinSpeed = spinnerScript.spinSpeed;
+
+            spinSpeedBar_Image.fillAmount = _currentSpinSpeed / _startSpinSpeed;
+            SpinSpeedRatio_Text.text = _currentSpinSpeed.ToString("F0") + "/" + _startSpinSpeed;
+
+            if (_currentSpinSpeed < 100)
+            {
+                //Die.
+                Dead();
+            }
         }
-        else if (isDefender)
+        
+    }
+
+    void Dead()
+    {
+        _isDead = true;
+        GetComponent<MovementController>().enabled = false;
+        rb.freezeRotation = false;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+
+        spinnerScript.spinSpeed = 0f;
+        uI_3DGameObject.SetActive(false);
+
+        if (photonView.IsMine)
         {
-            _damageAmount *= getDamageCoefficient_Defender;
+            //countdown to respown!
+            StartCoroutine(ReSpawn());
         }
-        spinnerScript.spinSpeed -= _damageAmount;
+
+    }
+
+    IEnumerator ReSpawn()
+    {
+        GameObject canvasGameObject = GameObject.Find("Canvas");
+
+        if(_deathPanelUIGameObject == null)
+        {
+            _deathPanelUIGameObject = Instantiate(deathPanelUIPrefab, canvasGameObject.transform);
+        } else
+        {
+            _deathPanelUIGameObject.SetActive(true);
+        }
+
+        Text respawnTimeText = _deathPanelUIGameObject.transform.Find("RespawnTimeText").GetComponent<Text>();
+
+        float respawnTime = 8.0f;
+
+        respawnTimeText.text = respawnTime.ToString(".00");
+
+        while (respawnTime > 0.0f)
+        {
+            yield return new WaitForSeconds(1.0f);
+            respawnTime -= 1.0f;
+            respawnTimeText.text = respawnTime.ToString(".00");
+
+            GetComponent<MovementController>().enabled = false;
+        }
+
+        _deathPanelUIGameObject.SetActive(false);
+        GetComponent<MovementController>().enabled = true;
+
+        photonView.RPC("ReBorn", RpcTarget.AllBuffered);
+
+    }
+    [PunRPC]
+    public void ReBorn()
+    {
+        spinnerScript.spinSpeed = _startSpinSpeed;
         _currentSpinSpeed = spinnerScript.spinSpeed;
 
         spinSpeedBar_Image.fillAmount = _currentSpinSpeed / _startSpinSpeed;
-        SpinSpeedRatio_Text.text = _currentSpinSpeed.ToString("F0") + "/" + _startSpinSpeed; 
+        SpinSpeedRatio_Text.text = _currentSpinSpeed + "/" + _startSpinSpeed;
+
+        rb.freezeRotation = true;
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+
+        uI_3DGameObject.SetActive(true);
+
+        _isDead = false;
+
     }
 
     void Start()
     {
         CheckPlayerType();
+
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
